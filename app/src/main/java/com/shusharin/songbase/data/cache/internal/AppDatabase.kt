35fcf3a58f80b8e607d428ff0a95f1f242738a91
Alexandr Shusharin <ashusharin.dev@gmail.com>
2022-01-31 20:55:31 +0500

    Fix database

---
+++ app/src/main/java/com/shusharin/songbase/data/cache/internal/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.shusharin.songbase.data.cache.internal
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [SongDb::class,], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun dao(): DAO
+}
