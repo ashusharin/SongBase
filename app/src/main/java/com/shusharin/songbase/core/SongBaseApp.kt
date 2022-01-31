1456340aa3c101f898251edbfc91fcd9834531ae
Alexandr Shusharin <ashusharin.dev@gmail.com>
2022-01-31 20:59:52 +0500

    Fix custom application

--- app/src/main/java/com/shusharin/songbase/core/SongBaseApp.kt
+++ app/src/main/java/com/shusharin/songbase/core/SongBaseApp.kt
@@ -1,6 +1,43 @@
 package com.shusharin.songbase.core
 
 import android.app.Application
+import androidx.room.Room
+import com.shusharin.songbase.data.SongRepository
+import com.shusharin.songbase.data.ToSongMapper
+import com.shusharin.songbase.data.cache.external.CursorManager
+import com.shusharin.songbase.data.cache.external.ResolverWrapper
+import com.shusharin.songbase.data.cache.external.ExternalCacheDataSource
+import com.shusharin.songbase.data.cache.internal.*
+import com.shusharin.songbase.domain.BaseSongDataListToDomainMapper
+import com.shusharin.songbase.domain.BaseSongDataToDomainMapper
+import com.shusharin.songbase.domain.SongsInteractor
+import com.shusharin.songbase.ui.*
 
 class SongBaseApp : Application() {
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate() {
+        super.onCreate()
+        val content = ResolverWrapper.Base(this)
+        val appDatabase = Room
+            .databaseBuilder(this, AppDatabase::class.java, "Song.db")
+            .fallbackToDestructiveMigration()
+            .build()
+        val dao = appDatabase.dao()
+        val songDatabase = SongDatabase(dao)
+        val cacheDataSource = InternalCacheDataSource.Base(songDatabase, SongDataToDbMapper.Base())
+        val cursorManager = CursorManager.Base()
+        val externalCacheDataSource = ExternalCacheDataSource.Base(content,cursorManager)
+        val toSongMapper = ToSongMapper.Base()
+        val cacheMapper = SongListCacheMapper.Base(toSongMapper)
+        val songRepository =
+            SongRepository.Base(cacheDataSource, externalCacheDataSource, cacheMapper)
+        val songsInteractor = SongsInteractor.Base(songRepository,
+            BaseSongDataListToDomainMapper(BaseSongDataToDomainMapper()))
+        val communication = SongListCommunication.Base()
+        viewModel = MainViewModel(songsInteractor,
+            communication,
+            BaseSongDomainListToUiMapper(BaseSongDomainToUiMapper(), ResourceProvider.Base(this)))
+
+    }
 }
